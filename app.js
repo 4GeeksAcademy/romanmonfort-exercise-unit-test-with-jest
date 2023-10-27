8baa54301544da6a615e77a85292d0bb6f4b3976
romanmonfort <137888802+romanmonfort@users.noreply.github.com>
2023-10-27 23:25:54 +0000

    push

---
+++ app.js
@@ -0,0 +1,33 @@
+const fromEuroToDollar = function(valueInEuro) {
+    // Convertimos el valor a dólares
+    let valueInDollar = valueInEuro * 1.07;
+    // Retornamos el valor en dólares
+    return valueInDollar;
+}
+
+const fromDollarToYen = function(valueInDolar) {
+    // Convertimos el valor a dólares
+    let valueInYen = valueInDolar * 156.5;
+    // Retornamos el valor en dólares
+    return valueInYen;
+}
+
+const fromYenToPound = function(valueInYen) {
+    // Convertimos el valor a dólares
+    let valueInPound = valueInYen * 0.87;
+    // Retornamos el valor en dólares
+    return valueInPound;
+}
+
+
+
+const sum = (a,b) => {
+    return a + b
+}
+
+// Solo un registro en consola para nosotros
+console.log(sum(7,3))
+
+// Exporta la función para usarla en otros archivos 
+// (similar a la palabra clave "export" cuando se usa webpack)
+module.exports = { sum , fromEuroToDollar,fromYenToPound,fromDollarToYen};
